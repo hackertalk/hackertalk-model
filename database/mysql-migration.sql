59c594f2bb3616a8bbbf0bf4350cfe9d8fc09147
hdsuperman <zhd.superman@gmail.com>
2023-05-02 15:08:31 +0800

    feat: add schema

--- database/mysql-migration.sql
+++ database/mysql-migration.sql
@@ -1,21 +1,22 @@
-alter table "user" alter column verified type bool using verified::int::bool;
+alter table application."user" alter column verified type boolean using verified::int::boolean;
 
-alter table post alter column blocked type bool using blocked::int::bool;
-alter table post alter column deleted type bool using deleted::int::bool;
+alter table application.post alter column blocked type boolean using blocked::int::boolean;
+alter table application.post alter column deleted type boolean using deleted::int::boolean;
 
-alter table comment alter column blocked type bool using blocked::int::bool;
-alter table comment alter column deleted type bool using deleted::int::bool;
+alter table application.comment alter column blocked type boolean using blocked::int::boolean;
+alter table application.comment alter column deleted type boolean using deleted::int::boolean;
 
-alter table channel alter column blocked type bool using blocked::int::bool;
-alter table channel alter column deleted type bool using deleted::int::bool;
-alter table channel_message alter column blocked type bool using blocked::int::bool;
-alter table channel_message alter column deleted type bool using deleted::int::bool;
+alter table application.channel alter column blocked type boolean using blocked::int::boolean;
+alter table application.channel alter column deleted type boolean using deleted::int::boolean;
+alter table application.channel_message alter column blocked type boolean using blocked::int::boolean;
+alter table application.channel_message alter column deleted type boolean using deleted::int::boolean;
 
-alter table feedback alter column pending type bool using pending::int::bool;
+alter table application.feedback alter column pending type boolean using pending::int::boolean;
 
-alter table atom_notification alter column unread type bool using unread::int::bool;
-alter table comment_notification alter column unread type bool using unread::int::bool;
-alter table follow_notification alter column unread type bool using unread::int::bool;
-alter table like_notification alter column unread type bool using unread::int::bool;
-alter table system_notification alter column unread type bool using unread::int::bool;
+alter table application.atom_notification alter column unread type boolean using unread::int::boolean;
+alter table application.comment_notification alter column unread type boolean using unread::int::boolean;
+alter table application.follow_notification alter column unread type boolean using unread::int::boolean;
+alter table application.like_notification alter column unread type boolean using unread::int::boolean;
+alter table application.system_notification alter column unread type boolean using unread::int::boolean;
 
+alter table application.chat alter column deleted type boolean using deleted::int::boolean;
