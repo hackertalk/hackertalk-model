bba732c43ea8a308132bc39da53a43b47e02fd31
hdsuperman <zhd.superman@gmail.com>
2024-01-19 11:34:53 +0800

    refactor: remove javax

--- src/main/java/net/hackertalk/model/talk/TalkLike.java
+++ src/main/java/net/hackertalk/model/talk/TalkLike.java
@@ -3,9 +3,8 @@ package net.hackertalk.model.talk;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import jakarta.persistence.*;
 import lombok.*;
-
-import javax.persistence.*;
 
 @Entity
 @Getter
